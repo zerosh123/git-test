8d9a06903b9a9770e2170b96933c142ec5a13fba
caolingling <2569947068@qq.com>
2020-08-04 21:09:25 +0800

    把idea代码提交到本地仓库

---
+++ src/com/itheima/git/FirstGit.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
