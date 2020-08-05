c3c4b1da079d1d0a94b3d55199944e5bf094cbb7
zerosh123 <69186840+zerosh123@users.noreply.github.com>
2020-08-05 10:26:42 +0800

    Update FirstGit.java

--- src/com/itheima/git/FirstGit.java
+++ src/com/itheima/git/FirstGit.java
@@ -4,8 +4,8 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello");
+        System.out.println("hello");
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
