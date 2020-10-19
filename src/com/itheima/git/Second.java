1e2bfdd2a31141b12ebdcb433e9980f49fc57e59
caolingling <2569947068@qq.com>
2020-10-19 21:39:07 +0800

    对second做了一次修改

--- src/com/itheima/git/Second.java
+++ src/com/itheima/git/Second.java
@@ -1,8 +1,16 @@
 package com.itheima.git;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Second {
     public void test(){
         System.out.println("jijjfoefoief");
+    }
+    public void getAll(){
+        List list=new ArrayList();
+        list.add("hahhah");
+        list.add("xixiix");
     }
 
 }
